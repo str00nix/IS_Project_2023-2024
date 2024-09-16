4e0ddf01f89fa8b01f6d549156a9149da99baf9b
str00nix <zivkosh@yahoo.com>
2024-09-16 13:24:47 +0200

    Added packages needed for exporting into excel spreadsheet format and pdf format

--- AdminApplication/AdminApplication/Controllers/PlaylistController.cs
+++ AdminApplication/AdminApplication/Controllers/PlaylistController.cs
@@ -1,4 +1,5 @@
 ﻿using AdminApplication.Models;
+using GemBox.Document;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http;
@@ -8,6 +9,11 @@ namespace AdminApplication.Controllers
 {
     public class PlaylistController : Controller
     {
+        public PlaylistController()
+        {
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -25,6 +31,20 @@ namespace AdminApplication.Controllers
             List<PlaylistDTO>? result = JsonConvert.DeserializeObject<List<PlaylistDTO>>(json);
 
             return result;
+        }
+
+        [HttpGet, ActionName("ExportAsExcel"), Produces("application/json")]
+        public async Task<List<PlaylistDTO>?> ExportPlaylistsAsExcelSpreadsheet()
+        {
+            //HttpClient client = new HttpClient();
+
+            //string URL = "http://localhost:5027/api/Admin/GetAllPlaylistDTOs";
+
+            //var json = await client.GetStringAsync(URL);
+
+            //List<PlaylistDTO>? result = JsonConvert.DeserializeObject<List<PlaylistDTO>>(json);
+
+            //return result;
         }
 
     }
